738c4fd9aa07b543d60c69b2d6c1dd679f912aab
dygreen <ehdus9877@naver.com>
2022-08-11 21:48:52 +0900

    files add

--- 1_basic.js
+++ 1_basic.js
@@ -4,3 +4,19 @@ var name1 = 'kim';
 var name2 = ['kim', 'park'];
 var name3 = { name: 'Kim' };
 // let name3: { name?: string } = { name: 'Kim' }; ?=옵션이라는 뜻
+// 2. 다양한 타입이 들어올 수 있게 하려면 : union type
+var name4 = 123;
+var name5 = 123;
+// 4. 함수에 타입지정 가능
+function one(x) {
+    return x * 2;
+}
+var john = [123, true];
+var john2 = { name: 'Kim', age: '123' };
+// 7. class 타입지정 가능
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
--- 1_basic.ts
+++ 1_basic.ts
@@ -8,3 +8,39 @@ let name2: string[] = ['kim', 'park'];
 let name3: { name: string } = { name: 'Kim' };
 // let name3: { name?: string } = { name: 'Kim' }; ?=옵션이라는 뜻
 
+
+// 2. 다양한 타입이 들어올 수 있게 하려면 : union type
+let name4: string | number = 123;
+
+
+// 3. 타입은 변수에 담아 쓸 수 있음 : type alias
+type Mytype = string | number;
+let name5: Mytype = 123;
+
+
+// 4. 함수에 타입지정 가능
+function one(x: number): number {
+  return x * 2
+}
+// -> 이 함수는 파라미터로 number, return 값으로 number를 반환해야 함
+
+
+// 5. array에 쓸 수 있는 tuple 타입
+type Member = [number, boolean]; // 무조건 첫 째는 number, 둘 째는 boolean
+let john: Member = [123, true];
+
+
+// 6. object에 타입지정해야 할 속성이 너무 많으면
+type Member2 = {
+  [key: string]: string, // 글자로 된 모든 object 속성의 타입은 :string
+}
+let john2: Member2 = { name: 'Kim', age: '123' };
+
+
+// 7. class 타입지정 가능
+class User {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
